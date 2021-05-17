5a4219010b64fdc4e7c8d8c6bf344e3541de6375
Hyunjoong Kim <hj.ben.kim@gmail.com>
2021-05-17 20:50:44 +0900

    add redis docker script

---
+++ redis/docker-script/container-init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+container_name=$1
+image_version=$2
+
+if [ -z $image_version ]
+then
+  echo "Some parameter value is empty. Usage: container-init.sh <container_name> <image_version>"
+  exit 1
+fi
+
+echo "Create Redis container."
+(docker run --privileged --name $container_name -d -p 6379:6379 -v /sys/fs/cgroup:/sys/fs/cgroup hjben/redis:$image_version)
+code=$?
+
+if [ $code -gt 0 ]
+then
+  echo "Error raised while creating container. If container exists, remove the container using container-remove.sh file."
+  exit 1
+fi
+
+echo "Done."
+sleep 2
+
+docker exec -it $container_name bash -c "/sh/init.sh"
