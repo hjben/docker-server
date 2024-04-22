cdf9f339cfea92f9c58f9f5fad040e780e2a9b52
김현중 <hj.ben.kim@gmail.com>
2024-04-22 22:24:27 +0900

    Update init.sh
    revert commit

--- mariadb/sh/init.sh
+++ mariadb/sh/init.sh
@@ -8,11 +8,5 @@ systemctl enable mariadb
 
 root_password=$MARIADB_ROOT_PASSWORD
 /sh/set-root-password.sh $root_password init
-
-cat << EOF > /sh/grant.sql
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$maria_root_password';
-EOF
-
-mysql -u root -p < /sh/grant.sql
       
 echo "Initialization done."
